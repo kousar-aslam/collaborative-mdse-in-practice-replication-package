684190ddffe9775fdcf304de1123d11ac890c3ff
david-istvan <goerenh@gmail.com>
2022-03-16 17:51:49 -0400

    Factored out file location

--- 04_analysis/prepare.py
+++ 04_analysis/prepare.py
@@ -4,7 +4,9 @@ import csv
 def sensitive_data():
     return [(182, 4)]
 
-workbook = openpyxl.load_workbook('../03_data/latest/data.xlsx')
+fileLocation = '../03_data/latest'
+    
+workbook = openpyxl.load_workbook('{}/data.xlsx'.format(fileLocation))
 sheet = workbook.active
 
 #Remove sensitive information
@@ -12,7 +14,7 @@ for (col, colNum) in sensitive_data():
     sheet.delete_cols(col, colNum)
     
 #Save as CSV
-with open('../03_data/latest/data.csv', 'w', newline="") as file_handle:
+with open('{}/data.csv'.format(fileLocation), 'w', newline="") as file_handle:
     csv_writer = csv.writer(file_handle, delimiter=';')
     for row in sheet.iter_rows():
         csv_writer.writerow([cell.value for cell in row])
