541aa2b403f925b1a9b438ebfb3006edd94bf797
david-istvan <goerenh@gmail.com>
2022-04-01 11:59:33 -0400

    Created a separate folder for the classification framework, shifting everything accordingly

---
+++ 04_data/00_remove_sensitive_information.py
@@ -0,0 +1,16 @@
+import openpyxl
+import csv
+
+def sensitive_data():
+    return [(182, 4)]
+    
+fileLocation = '../04_data/questionnaire'
+    
+workbook = openpyxl.load_workbook('{}/questionnaire_data_raw.xlsx'.format(fileLocation))
+sheet = workbook.active
+
+#Remove sensitive information
+for (col, colNum) in sensitive_data():
+    sheet.delete_cols(col, colNum)
+
+workbook.save(filename='{}/questionnaire_data.xlsx'.format(fileLocation))
